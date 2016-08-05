cdae87200fdd3a2516e75f7d64cfd8a1607b70b4
Anthony Ng <newyork.anthonyng@gmail.com>
2016-08-05 13:07:37 -0400

    Finish tutorial

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -14,7 +14,7 @@ describe('reducer', () => {
 					pair: List.of('Trainspotting', '28 Days Later'),
 					tally: Map({ Trainspotting: 1 })
 				})
-			});
+			})
 		};
 		const nextState = reducer(initialState, action);
 
@@ -35,7 +35,7 @@ describe('reducer', () => {
 					pair: ['Trainspotting', '28 Days Later'],
 					tally: { Trainspotting: 1 }
 				}
-			};
+			}
 		};
 		const nextState = reducer(initialState, action);
 
@@ -55,7 +55,7 @@ describe('reducer', () => {
 					pair: ['Trainspotting', '28 Days Later'],
 					tally: { Trainspotting: 1 }
 				}
-			};
+			}
 		};
 		const nextState = reducer(undefined, action);
 
@@ -63,6 +63,68 @@ describe('reducer', () => {
 			vote: {
 				pair: ['Trainspotting', '28 Days Later'],
 				tally: { Trainspotting: 1 }
+			}
+		}));
+	});
+
+	it('handles VOTE by setting hasVoted', () => {
+		const state = fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 Days Later'],
+				tally: { Trainspotting: 1 }
+			}
+		});
+		const action = { type: 'VOTE', entry: 'Trainspotting' };
+		const nextState = reducer(state, action);
+
+		expect(nextState).to.equal(fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 Days Later'],
+				tally: { Trainspotting: 1 }
+			},
+			hasVoted: 'Trainspotting'
+		}));
+	});
+
+	it('does not set hasVoted for VOTE on invalid entry', () => {
+		const state = fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 Days Later'],
+				tally: { Trainspotting: 1 }
+			}
+		});
+		const action = { type: 'VOTE', entry: 'Sunshine' };
+		const nextState = reducer(state, action);
+
+		expect(nextState).to.equal(fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 Days Later'],
+				tally: { Trainspotting: 1 }
+			}
+		}));
+	});
+
+	it('removes hasVoted on SET_STATE if pair changes', () => {
+		const initialState = fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 Days Later'],
+				tally: { Trainspotting: 1 }
+			},
+			hasVoted: 'Trainspotting'
+		});
+		const action = {
+			type: 'SET_STATE',
+			state: {
+				vote: {
+					pair: ['Sunshine', 'Slumdog Millionaire']
+				}
+			}
+		};
+		const nextState = reducer(initialState, action);
+
+		expect(nextState).to.equal(fromJS({
+			vote: {
+				pair: ['Sunshine', 'Slumdog Millionaire']
 			}
 		}));
 	});
